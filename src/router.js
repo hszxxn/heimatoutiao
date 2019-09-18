97dba125d348b3cb93035861328e264a2243fdaa
hsz <44550238@qq.com>
2019-09-18 10:14:54 +0800

    登录和主页页面分析

--- src/router.js
+++ src/router.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/home/'
-
+import Home from './views/home'
+import Login from './views/login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/home',
+      component: Home
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
       component: Home
     }
     // {
