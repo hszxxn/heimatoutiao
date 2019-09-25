09178385c5ec7fdc4ef920dabc545fa01d40fb7d
hsz <44550238@qq.com>
2019-09-25 20:49:11 +0800

    账户信息页面结构完成

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,6 +3,24 @@
       <break-crumb slot='header'>
           <template slot='title'>账户信息</template>
       </break-crumb>
+      <el-form label-width='100px'>
+          <el-form-item label='账户信息'>
+              <el-input style='width:300px'></el-input>
+          </el-form-item>
+          <el-form-item label='简介'>
+              <el-input style='width:300px'></el-input>
+          </el-form-item>
+          <el-form-item label='邮箱'>
+               <el-input style='width:300px'></el-input>
+          </el-form-item>
+          <el-form-item label='手机号'>
+              <el-input style='width:300px'></el-input>
+          </el-form-item>
+          <el-form-item>
+              <el-button type='primary'>保存信息</el-button>
+          </el-form-item>
+          <div class='photo'></div>
+      </el-form>
   </el-card>
 </template>
 
@@ -12,6 +30,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.el-form {
+    margin: 20px 100px !important;
+    position: relative;
+    .photo {
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        border: 1px solid #ccc;
+        position: absolute;
+        right: 250px;
+        top: 0px
+
+    }
+}
 
 </style>
