6a897ec09c515a55e6221c753033ee7cd1493392
hsz <44550238@qq.com>
2019-09-25 21:03:32 +0800

    账户信息页面数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -4,29 +4,59 @@
           <template slot='title'>账户信息</template>
       </break-crumb>
       <el-form label-width='100px'>
-          <el-form-item label='账户信息'>
-              <el-input style='width:300px'></el-input>
+          <el-form-item label='名称'>
+              <el-input style='width:300px' v-model='formData.name'></el-input>
           </el-form-item>
           <el-form-item label='简介'>
-              <el-input style='width:300px'></el-input>
+              <el-input style='width:300px' v-model='formData.intro'></el-input>
           </el-form-item>
           <el-form-item label='邮箱'>
-               <el-input style='width:300px'></el-input>
+               <el-input style='width:300px' v-model='formData.email'></el-input>
           </el-form-item>
           <el-form-item label='手机号'>
-              <el-input style='width:300px'></el-input>
+              <el-input style='width:300px' v-model="formData.mobile"></el-input>
           </el-form-item>
           <el-form-item>
               <el-button type='primary'>保存信息</el-button>
           </el-form-item>
-          <div class='photo'></div>
+          <div class='photo'>
+              <img :src="formData.img?formData.img:defaultImg" alt="">
+          </div>
       </el-form>
   </el-card>
 </template>
-
 <script>
 export default {
-
+  data () {
+    return {
+      formData: {
+        name: '',
+        intro: '',
+        email: '',
+        mobile: '',
+        img: ''
+      },
+      defaultImg: require('../../assets/img/default.jpg')
+    }
+  },
+  methods: {
+    //    获取用户信息
+    getUserInfo () {
+      this.$http({
+        url: 'user/profile'
+      }).then((result) => {
+        console.log(result)
+        this.formData.name = result.data.name
+        this.formData.intro = result.data.intro
+        this.formData.email = result.data.email
+        this.formData.mobile = result.data.mobile
+        this.formData.img = result.data.photo
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
@@ -41,8 +71,12 @@ export default {
         border: 1px solid #ccc;
         position: absolute;
         right: 250px;
-        top: 0px
-
+        top: 0px;
+        overflow: hidden;
+        img{
+            width: 100%;
+            height: 100%;
+        }
     }
 }
 
